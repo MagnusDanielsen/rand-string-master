9f16e034abcfcf458d79a50150185023e9fc45ca
Magnus Danielsen <38593596+MagnusDanielsen@users.noreply.github.com>
2019-12-02 10:48:10 +0100

    Update randstring.c

--- randstring.c
+++ randstring.c
@@ -1,20 +1,22 @@
+// Randstring.c
+// Last edited 02.12.2019
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void rand_str(char *, size_t);
-
 int main(void) {
-    char str[] = { [41] = '\1' }; // make the last character non-zero so we can test based on it later
+    char str[] = { [] = '\1' }; // make the last character non-zero so we can test based on it later
     rand_str(str, sizeof str - 1);
-    assert(str[41] == '\0');      // test the correct insertion of string terminator
+    assert(str[26] == '\0');  // test the correct insertion of string terminator
     puts(str);
 }
 
 void rand_str(char *dest, size_t length) {
     char charset[] = "0123456789"
                      "abcdefghijklmnopqrstuvwxyz"
-                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                     "+=&%¤#@£$€*<>-.,_";
 
     while (length-- > 0) {
         size_t index = (double) rand() / RAND_MAX * (sizeof charset - 1);
