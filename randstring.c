ad27d8192d516c7f8c4966ed6248123a487b7add
Magnus Danielsen <38593596+MagnusDanielsen@users.noreply.github.com>
2020-10-08 05:25:37 +0200

    Update randstring.c
    Final release of randstring.

--- randstring.c
+++ randstring.c
@@ -1,8 +1,23 @@
 // Randstring.c
-// Last edited 02.12.2019
+// Last edit 02.12.2019
+// final edit and release 08.10.2020
+
+/*
+ *  Randstring.c:  Fully automated string generator (linux based)
+ *
+ *  Copyright Magnus Danielsen (18 February 2019-2020)
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2.
+ *
+ */
+
+// INCLUDES
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void rand_str(char *, size_t);
 int main(void) {
@@ -12,7 +27,7 @@ int main(void) {
     puts(str);
 }
 
-void rand_str(char *dest, size_t length) {
+void rand_str(char *dest, size_t length) {   //generates a random string using the charset
     char charset[] = "0123456789"
                      "abcdefghijklmnopqrstuvwxyz"
                      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -24,3 +39,4 @@ void rand_str(char *dest, size_t length) {
     }
     *dest = '\0';
 }
+// END
